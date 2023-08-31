c34c67222e62721281498d891d1580af6c1026a0
Hayoung Shim <gkdud3579@gmail.com>
2023-08-31 16:34:32 +0900

    solved p1008

---
+++ Basic_C/10998.c
@@ -0,0 +1,15 @@
+//https://www.acmicpc.net/problem/10998
+
+#include <stdio.h>
+
+int main(void)
+{
+    int A, B;
+
+    scanf("%d %d", &A, &B);
+
+    int result = A * B;
+    printf("%d\n", result);
+
+    return 0;
+}
