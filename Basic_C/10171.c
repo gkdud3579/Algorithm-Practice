8bb0563635386e097c183f25c855637e050f3a72
Hayoung Shim <gkdud3579@gmail.com>
2023-09-03 17:32:24 +0900

    print cat

---
+++ Basic_C/10171.c
@@ -0,0 +1,12 @@
+//https://www.acmicpc.net/problem/10171
+
+#include <stdio.h>
+
+int main(void) {
+    printf("\\    /\\\n");
+    printf(" )  ( ')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|\n");
+
+    return 0;
+}
