82b933cfd80ad7c8d5fafa256c440c7c7f37d233
Hayoung Shim <gkdud3579@gmail.com>
2023-09-03 17:42:24 +0900

    print dog

---
+++ Basic_C/10172.c
@@ -0,0 +1,14 @@
+//https://www.acmicpc.net/problem/10172
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("|\\_/|\n");
+    printf("|q p|   /}\n");
+    printf("( 0 )\"\"\"\\\n");
+    printf("|\"^\"`    |\n");
+    printf("||_/=\\\\__|\n");
+
+    return 0;
+}
