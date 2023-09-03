55502eeb41ffc5ab9c4303435722a335056c2e6e
Hayoung Shim <gkdud3579@gmail.com>
2023-09-03 16:21:12 +0900

    solved p18108

---
+++ Basic_C/18108.c
@@ -0,0 +1,18 @@
+//https://www.acmicpc.net/problem/18108
+
+#include <stdio.h>
+
+int main (void)
+{
+    int BKyear;
+
+    scanf("%d", &BKyear);
+    
+    int year;
+
+    year = BKyear - 543;
+
+    printf("%d\n", year);
+
+    return 0;
+}
