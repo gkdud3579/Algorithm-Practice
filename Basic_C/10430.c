13ca2b2d8bff3dbac6d6d12f9131200cb3705872
Hayoung Shim <gkdud3579@gmail.com>
2023-09-03 16:34:02 +0900

    solved p10430

---
+++ Basic_C/10430.c
@@ -0,0 +1,16 @@
+//https://www.acmicpc.net/problem/10430
+
+#include <stdio.h>
+
+int main(void)
+{
+    int A, B, C;
+
+    scanf("%d %d %d", &A, &B, &C);
+
+    printf("%d\n", (A + B) % C);
+    printf("%d\n", ((A % C) + (B % C) % C));
+    printf("%d\n", (A * B) % C);
+    printf("%d\n", ((A % C) * (B % C) % C));
+
+}
