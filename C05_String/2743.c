aee2451d9a2eb3ed76c01ee417e8fa18e078f7ac
Hayoung Shim <gkdud3579@gmail.com>
2023-09-14 16:52:23 +0900

    solved p2743

---
+++ C05_String/2743.c
@@ -0,0 +1,16 @@
+//https://www.acmicpc.net/problem/2743
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char word[101];
+    scanf("%s", word);
+
+    int length = strlen(word);
+
+    printf("%d\n", length);
+
+    return 0;
+}
