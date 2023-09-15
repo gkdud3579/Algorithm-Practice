184da19fbab60ad4ec38a394b5cfddcba866ab35
Hayoung Shim <gkdud3579@gmail.com>
2023-09-15 17:44:52 +0900

    solved p11654

---
+++ C05_String/11654.c
@@ -0,0 +1,14 @@
+//https://www.acmicpc.net/problem/11654
+
+#include <stdio.h>
+
+int main()
+{
+    char c;
+    scanf("%c", &c);
+
+    int ascii = c;
+    printf("%d\n", ascii);
+
+    return 0;
+}
