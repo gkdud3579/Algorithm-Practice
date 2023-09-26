8312fa790e339f315b38451524fcac25dc173499
Hayoung Shim <gkdud3579@gmail.com>
2023-09-26 22:47:10 +0900

    solved 2941

---
+++ C06_Advanced1/2941.c
@@ -0,0 +1,36 @@
+//https://www.acmicpc.net/problem/2941
+
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+    char word[101];
+    scanf("%s", word);
+    
+    int count = 0;
+    int len = strlen(word);
+    
+    for (int i = 0; i < len; i++) 
+    {
+        if (word[i] == 'c' && (word[i + 1] == '=' || word[i + 1] == '-'))
+            i++;
+        else if (word[i] == 'd') 
+        {
+            if (word[i + 1] == 'z' && word[i + 2] == '=')
+                i += 2;
+            else if (word[i + 1] == '-')
+                i++;
+        }
+        else if ((word[i] == 'l' && word[i + 1] == 'j') || (word[i] == 'n' && word[i + 1] == 'j'))
+            i++;
+        else if ((word[i] == 's' && word[i + 1] == '=') || (word[i] == 'z' && word[i + 1] == '='))
+            i++;
+        
+        count++;
+    }
+    
+    printf("%d\n", count);
+    
+    return 0;
+}
