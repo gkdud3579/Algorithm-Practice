58a85fea133a21a792e5a10cffcab23ae2612a22
Hayoung Shim <gkdud3579@gmail.com>
2023-09-27 16:31:49 +0900

    solved 7282

---
+++ C01_arithmetic_operations/7287.c
@@ -0,0 +1,13 @@
+//https://www.acmicpc.net/problem/7287
+
+#include <stdio.h>
+
+int main() {
+    int solvedProblems = 60;
+    char username[] = "gkdud3579"; // 예시: 자신의 아이디
+    
+    printf("%d\n", solvedProblems);
+    printf("%s\n", username);
+    
+    return 0;
+}
