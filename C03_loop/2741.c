0f9b3b13da2c5e0e559ff4ac409ca07e0fff358b
Hayoung Shim <gkdud3579@gmail.com>
2023-09-27 16:53:39 +0900

    solved 2741

---
+++ C03_loop/2741.c
@@ -0,0 +1,15 @@
+//https://www.acmicpc.net/problem/2741
+
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    for (int i = 1; i <= N; i++) 
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
