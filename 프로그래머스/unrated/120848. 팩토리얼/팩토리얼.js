022b812d44cc10b19f25e4e24f39a3b95a7730b5
Hayoung <76105226+gkdud3579@users.noreply.github.com>
2024-01-03 14:20:08 +0900

    [level unrated] Title: 팩토리얼, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120848. 팩토리얼/팩토리얼.js
+++ 프로그래머스/unrated/120848. 팩토리얼/팩토리얼.js
@@ -1,15 +1,14 @@
-function solution(n) {
-    let num = 1; 
-    while (factorial(num) <= n) {
+function solution(n){
+    let num = 1;
+    while(factorial(num) <= n)
         num++;
-    }
     return num - 1;
 }
 
-function factorial(num) {
+function factorial(num){
     let result = 1;
-    for (let i = 1; i <= num; i++) {
+    for(let i = 1; i <= num; i++)
         result *= i;
-    }
+    
     return result;
 }
