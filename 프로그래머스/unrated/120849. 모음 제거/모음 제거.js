dad42f9d023d1df02e8319344471f41c4a786f0d
Hayoung <76105226+gkdud3579@users.noreply.github.com>
2024-01-04 11:46:45 +0900

    [level unrated] Title: 모음 제거, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.js
@@ -1,12 +1,3 @@
-function solution(my_string) {
-    const vowels = ["a", "e", "i", "o", "u"];
-    let result = '';
-
-    for (let char of my_string) {
-        if (!vowels.includes(char)) {
-            result += char;
-        }
-    }
-
-    return result;
+function solution(my_string){
+    return my_string.replace(/[aeiou]/g,'');
 }
