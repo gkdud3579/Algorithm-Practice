4f02a0610d4e3cb97743bce3cb5542d31fdfc875
Hayoung <76105226+gkdud3579@users.noreply.github.com>
2024-01-09 12:29:23 +0900

    [level unrated] Title: 대문자와 소문자, Time: 0.27 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,11 +1,3 @@
-function solution(my_string) {
-    let result = '';
-
-    for (let i = 0; i < my_string.length; i++) {
-        result += (my_string[i] >= 'a' && my_string[i] <= 'z') 
-                  ? my_string[i].toUpperCase() 
-                  : my_string[i].toLowerCase();
-    }
-
-    return result;
+function solution(my_string){
+    return my_string.split('').map(n => n === n.toUpperCase() ? n.toLowerCase() : n.toUpperCase()).join('');
 }
