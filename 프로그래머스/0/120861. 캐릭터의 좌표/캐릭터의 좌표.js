8735488c55149f871fc24c88a49dc46bcae55f44
Hayoung <76105226+gkdud3579@users.noreply.github.com>
2024-01-18 14:14:10 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,14 +1,14 @@
-function solution(keyinput, board) {
+function solution(keyinput, board){
     let x = 0, y = 0;
     let maxX = Math.floor(board[0] / 2);
     let maxY = Math.floor(board[1] / 2);
-
+    
+    //키 값에 따라 배열 이동
     keyinput.forEach(key => {
-        if (key === "left" && x > -maxX) x--;
-        else if (key === "right" && x < maxX) x++;
-        else if (key === "up" && y < maxY) y++;
-        else if (key === "down" && y > -maxY) y--;
+        if(key === "up" && y < maxY) y++;
+        else if(key === "down" && y > -maxY) y--;
+        else if(key === "left" && x > -maxX) x--;
+        else if(key === "right" && x < maxX) x++;
     });
-
     return [x, y];
 }
