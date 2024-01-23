fca70ab1123114252d85d97130de10a6b94f7a1e
Hayoung <76105226+gkdud3579@users.noreply.github.com>
2024-01-23 11:40:23 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -1,12 +1,14 @@
 function solution(n) {
-    let number = 1;
-    for (let count = 1; count <= n; number++) {
-        if (number % 3 === 0 || number.toString().includes('3')) {
+    let number = 1; 
+    for(let count = 1; count <= n; number++){
+        
+        if(number % 3 === 0 || number.toString().includes('3'))
             continue;
-        }
-        if (count === n) {
+        
+        if(count === n)
             return number;
-        }
-        count++;
+        
+         count++;
     }
+   
 }
