79abaf9584912ab4a1018f5207ad4417058ff1ac
Hayoung <76105226+gkdud3579@users.noreply.github.com>
2024-01-23 10:55:12 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let number = 1;
+    for (let count = 1; count <= n; number++) {
+        if (number % 3 === 0 || number.toString().includes('3')) {
+            continue;
+        }
+        if (count === n) {
+            return number;
+        }
+        count++;
+    }
+}
