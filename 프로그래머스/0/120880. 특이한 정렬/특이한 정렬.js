c81c0e141cc362b05fa83c5957c1b0b8742550d5
Hayoung <76105226+gkdud3579@users.noreply.github.com>
2024-01-24 11:13:37 +0900

    [level 0] Title: 특이한 정렬, Time: 0.05 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,15 +1,11 @@
 function solution(numlist, n) {
     return numlist.sort((a, b) => {
-        // 두 수와 n과의 절대적 거리를 계산
         const distanceA = Math.abs(a - n);
         const distanceB = Math.abs(b - n);
-
-        // 거리가 같으면 더 큰 수를 앞에 배치
-        if (distanceA === distanceB) {
+        
+        if(distanceA === distanceB)
             return b - a;
-        }
-
-        // 거리가 다르면 더 가까운 수를 앞에 배치
-        return distanceA - distanceB;
+        else
+            return distanceA - distanceB;
     });
 }
