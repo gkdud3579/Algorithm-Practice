ebbd2009a773b6aba9da7a3c84f91d91a1174b82
Hayoung <76105226+gkdud3579@users.noreply.github.com>
2024-02-23 12:25:51 +0900

    [Silver V] Title: 셀프 넘버, Time: 248 ms, Memory: 12068 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.js
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.js
@@ -1,27 +1,23 @@
-// 생성자를 찾는 함수
+//1. 생성자 찾기
 function d(n) {
     let sum = n;
     while (n > 0) {
-        sum += n % 10; // 각 자리수를 더함
+        sum += n % 10
         n = Math.floor(n / 10);
     }
     return sum;
 }
-
-// 생성자가 있는 숫자를 추적하기 위한 배열
+//2. 생성자가 있는 숫자 추적하기 위한 배열
 const hasConstructor = Array(10001).fill(false);
 
-// 1부터 10000까지 모든 숫자에 대해 생성자가 있는지 확인
+//3. 1부터 10000까지 숫자 중 생성자가 있는지 추적
 for (let i = 1; i <= 10000; i++) {
     const constructor = d(i);
-    if (constructor <= 10000) {
-        hasConstructor[constructor] = true; // 생성자가 있음을 표시
-    }
+    if(constructor <= 100000)
+        hasConstructor[constructor] = true
 }
-
-// 셀프 넘버 출력
-for (let i = 1; i <= 10000; i++) {
-    if (!hasConstructor[i]) { // 생성자가 없는 숫자만 출력
+//4. 셀프 넘버 출력
+for(let i = 1; i <= 10000; i++) {
+    if(!hasConstructor[i])
         console.log(i);
-    }
 }
